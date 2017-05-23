282a89b63e800c0ad55afeae67f3eeff012fc78d
Sofoklis <sofos@aueb.gr>
2017-05-23 04:46:42 +0300

    Add files via upload

---
+++ jdbc/exercises/exercise1_student.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `exercise1_student` (
+  `am` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `surname` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`am`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
